9497bf72cd30fc3fe8e3762e0e6bdaad2ff0625d
danielmo <danielmo@backbase.com>
2019-10-01 15:24:33 -0400

    formatting fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import {Routes} from '@angular/router';
-import {PrivacyPolicyComponent} from "./privacy-policy/privacy-policy.component";
+import {PrivacyPolicyComponent} from './privacy-policy/privacy-policy.component';
 
 export const rootRouterConfig: Routes = [
   {path: '', redirectTo: 'auth', pathMatch: 'full'},
