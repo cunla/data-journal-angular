6c4162c47b7e01e25273d745b78acbe6c4b16a8a
danielmo <danielmo@backbase.com>
2019-10-09 12:48:19 -0400

    export csv

--- src/app/home/trips/list/trips-list.component.ts
+++ src/app/home/trips/list/trips-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {EMPTY_TRIP, TripInterface, TripsService} from '../trips.service';
+import {Csvtools} from '../../common/csvtools.service';
+import {saveAs} from 'file-saver';
 
 @Component({
   selector: 'app-trips',
@@ -21,6 +23,16 @@ export class TripsListComponent implements OnInit {
     const value = this.searchValue.toLowerCase();
     this.trips.init('trips', 'start', {
       reverse: true, prepend: false, searchValue: value,
+    });
+  }
+
+  exportCsv() {
+    this.trips.data.subscribe(res => {
+      const tripsCsv = Csvtools.convertToCSV(res,
+        ['start', 'end', 'country', 'state', 'city', 'purpose']);
+      console.log(tripsCsv);
+      const blob = new Blob([tripsCsv], {type: 'text/plain;charset=utf-8'});
+      saveAs(blob, 'data.csv');
     });
   }
 }
