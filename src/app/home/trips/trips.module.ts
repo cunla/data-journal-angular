de2df1fc1243fabd304df9080298ebb108941c29
danielmo <danielmo@backbase.com>
2020-02-12 14:33:32 -0500

    formatting

--- src/app/home/trips/trips.module.ts
+++ src/app/home/trips/trips.module.ts
@@ -11,18 +11,17 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
 import {HomeGuard} from '../guard/home.guard';
 import {HomeGuardModule} from '../guard/home.guard.module';
-import {ToolsModule} from "../common/tools.module";
+import {ToolsModule} from '../common/tools.module';
 import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from "@angular/material/button";
-import {MatInputModule} from "@angular/material/input";
-import {MatSliderModule} from "@angular/material/slider";
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatSelectModule} from "@angular/material/select";
-import {MatListModule} from "@angular/material/list";
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSelectModule} from '@angular/material/select';
+import {MatListModule} from '@angular/material/list';
 import {AngularFireModule} from '@angular/fire';
 import {environment} from '../../../environments/environment';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireAuthModule} from '@angular/fire/auth';
 
 const routes: Routes = [
   {path: '', redirectTo: 'list', pathMatch: 'full'},
